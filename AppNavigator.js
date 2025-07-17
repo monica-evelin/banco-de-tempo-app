502ee3134598d9a6bb66badea7a24b3dd75ed926
Monica Oliveira <monica.evelin@hotmail.com>
2025-07-17 12:08:54 +0100

    Edição Navigator, Auth e MainTab

--- AppNavigator.js
+++ AppNavigator.js
@@ -8,18 +8,31 @@ import Login from "./screens/Login";
 import Signup from "./screens/Signup";
 import DetailsScreen from "./screens/DetailsScreen";
 import MainTabs from "./MainTabs";
+import { useAuth } from "./context/AuthContext";
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
+  const { user } = useAuth(); // Pega o usuário atual
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Signup" component={Signup} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="MainTabs" component={MainTabs} />
-        <Stack.Screen name="createAppointment" component={createAppointment} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {user ? (
+          <>
+            <Stack.Screen name="MainTabs" component={MainTabs} />
+            <Stack.Screen
+              name="createAppointment"
+              component={createAppointment}
+            />
+            <Stack.Screen name="Details" component={DetailsScreen} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Signup" component={Signup} />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
