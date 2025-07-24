f886c51ed85fa73fb4aca1d420d01fb228f2370b
Tatiane Oliveira <tatynha.araujo29@gmail.com>
2025-07-24 12:21:41 +0100

    Alteração no AuthContext

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,30 +1,3 @@
-/*import React, { createContext, useContext, useEffect, useState } from "react";
-import { onAuthStateChanged, signOut } from "firebase/auth";
-import { auth } from "../firebaseConfig";
-
-const AuthContext = createContext();
-
-export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (usr) => {
-      setUser(usr);
-    });
-
-    return () => unsubscribe();
-  }, []);
-
-  const logout = () => signOut(auth);
-
-  return (
-    <AuthContext.Provider value={{ user, setUser, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
-
-export const useAuth = () => useContext(AuthContext);*/
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
